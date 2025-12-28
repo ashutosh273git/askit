a60e993272e1a96fd095233cd309cd081c4ff07c
Ashutosh Gupta <ashutoshgupta2737@gmail.com>
2025-12-28 20:23:56 +0530

    corrected a bug connectDB function was not there in get ticket routes

--- app/api/ticket/get-all-tickets/route.ts
+++ app/api/ticket/get-all-tickets/route.ts
@@ -1,28 +1,24 @@
 import { auth } from "@/lib/auth";
+import connectDB from "@/lib/db";
 import Ticket from "@/models/Ticket";
 import { headers } from "next/headers";
 import { NextResponse } from "next/server";
 
 export async function GET() {
+  await connectDB();
   try {
     const session = await auth.api.getSession({
       headers: await headers(),
     });
 
-    if(!session){
-        return NextResponse.json(
-            {error: "Unauthorized"},
-            {status: 401}
-        )
+    if (!session) {
+      return NextResponse.json([], { status: 200 });
     }
 
-    const tickets = await Ticket.find().sort({createdAt: -1})
+    const tickets = await Ticket.find().sort({ createdAt: -1 });
 
-    return NextResponse.json(tickets, {status: 200})
+    return NextResponse.json(tickets, { status: 200 });
   } catch (error) {
-    return NextResponse.json(
-      { error: "Failed to fetch tickets" },
-      { status: 500 }
-    );
+    return NextResponse.json([], { status: 200 });
   }
 }
--- app/api/ticket/get-your-tickets/route.ts
+++ app/api/ticket/get-your-tickets/route.ts
@@ -1,9 +1,11 @@
 import { auth } from "@/lib/auth";
+import connectDB from "@/lib/db";
 import Ticket from "@/models/Ticket";
 import { headers } from "next/headers";
 import { NextResponse } from "next/server";
 
 export async function GET() {
+  await connectDB();
   try {
     const session = await auth.api.getSession({
       headers: await headers(),
